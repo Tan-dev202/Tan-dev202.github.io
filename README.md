e19589f563bc0edd906483008061d0f0be58372a
Enock Tangus <tanguskipngeno@gmail.com>
2025-03-02 16:22:28 +0300

    add project files

---
+++ README.md
@@ -0,0 +1,8 @@
+# My Personal Website
+This is a personal project covering topics learned in HTML and CSS.
+The project has the following sections:
+- [x] Nav bar
+- [x] Hero section
+- [x] Reviews section
+- [x] Contact section
+- [x] Footer section
---
+++ index.js
@@ -0,0 +1,23 @@
+const input = document.getElementById('btn');
+
+function clickAlert() {
+  alert('Redirecting...');
+}
+
+input.addEventListener('click', clickAlert);
+
+const git = document.getElementById('github');
+
+function clickAlert() {
+  alert('Redirecting...');
+}
+
+git.addEventListener('click', clickAlert);
+
+const mail = document.getElementById('email');
+
+function clickAlert() {
+  alert('Redirecting...');
+}
+
+mail.addEventListener('click', clickAlert);
